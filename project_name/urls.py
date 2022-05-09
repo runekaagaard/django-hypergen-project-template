6798c182450cb0b81a602cecaf11c37737ced7fb
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-09 15:14:41 +0200

    urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import hellohypergen.urls
+import hellohypergen.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hellohypergen/', hellohypergen.urls),
+    path('hellohypergen/', hellohypergen.views, name="hellohypergen"),
 ]
