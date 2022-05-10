81f32e2010a6eda78c79669e801fc2a6ae7147f1
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-10 11:43:22 +0200

    docs

--- hypergen_first_app/views.py
+++ hypergen_first_app/views.py
@@ -13,40 +13,53 @@ def base_template():
     """
     This base template is meant to be shared between your views.
     """
-    doctype()
-    with html():
+    doctype()  # all html elements are available as functions.
+    with html():  # elements can nest.
         with head():
             if title:
-                title("Hello {{ project_name }}")
-            script(src=static("hypergen/hypergen.min.js"))
+                title(
+                    "Hello {{ project_name }}")  # arguments to elements becomes the html innerText of the element.
+            script(src=static(
+                "hypergen/hypergen.min.js"))  # keyword arguments becomes html attributes of the element.
             link("https://unpkg.com/simpledotcss@2.0.7/simple.min.css")
-        with body():  # warning, don't set the id here, does not work!
+        with body():  # warning, don't set the target_id directly on the body element, does not work!
             h1("Hello {{ project_name }}")
-            p(i("Congratulations on running your very first Django Hypergen Project!"))
+            p(i("Congratulations on running your very first Django Hypergen Project!")
+             )  # elements can take elements.
 
             with div(id_="content"):  # see target_id below.
                 # The html triggered inside your views will appear here.
                 yield
+
             h1("Where to go from here?")
             with ul():
-                li("Play around with the source at", code("./hypergen_first_app/views.py"), sep=" ")
+                li(
+                    "Play around with the source at",
+                    code("./hypergen_first_app/views.py"),
+                    sep=" ",  # arguments are joined by a " " separator.
+                )
                 li("Check out the", a("documentation", href="https://hypergen.it/documentation/"), sep=" ")
                 li("Go crazy 24/7!")
 
 def content_template(encrypted_message=None):
     """
-    This template is specific to your view and the callbacks belonging to it.
+    This template is specific to your view and the callbacks belonging to it. Composes just like React functions.
     """
     p("Top secret agent? Encrypt your message with a super secret key:")
-    input_(id_="message", oninput=callback(my_callback, THIS))  # call "my_callback" on each oninput event.
+    input_(
+        id_="message",
+        # call "my_callback" on each oninput event.
+        # callback() takes all normal python datatypes and hypergen html elements as input.
+        oninput=callback(my_callback, THIS),  # 'THIS' means the value of the element it self.
+    )
     pre(code(encrypted_message if encrypted_message else "Type something, dammit!"))
 
-# Views - one view is normally an entire app with callbacks as actions.
+# Views - one view normally have multiple callbacks.
 
 @hypergen_view(perm=NO_PERM_REQUIRED, base_template=base_template)
 def my_view(request):
     """
-    Views renders html.
+    Views renders html and binds frontend events to callbacks.
     """
     content_template()
 
@@ -55,6 +68,10 @@ def my_view(request):
 @hypergen_callback(perm=NO_PERM_REQUIRED, target_id="content")
 def my_callback(request, message):
     """
-    Tells the frontend to put the output of content_template into the 'content' div.
+    Callbacks processes frontend events.
+    
+    This callback tells the frontend to put the output of content_template into the 'content' div.
+
+    The 'message' arg is the value of the <input> element.
     """
     content_template(codecs.encode(message if message is not None else "", 'rot_13'))
