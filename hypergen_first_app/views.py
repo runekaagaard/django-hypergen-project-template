f2a1ba266b94b8f75edb9f758624ce9082b7ec94
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-10 11:13:14 +0200

    views

--- hypergen_first_app/views.py
+++ hypergen_first_app/views.py
@@ -6,7 +6,7 @@ from hypergen.contrib import hypergen_view, hypergen_callback, NO_PERM_REQUIRED
 
 from django.templatetags.static import static
 
-# Templates - as you app grows you probably want to move them to a templates.py file.
+# Templates - as your app grows you probably want to move them to a templates.py file.
 
 @contextmanager  # base templates must be context managers that yields where the main content will be.
 def base_template():
@@ -20,9 +20,9 @@ def base_template():
                 title("Hello {{ project_name }}")
             script(src=static("hypergen/hypergen.min.js"))
             link("https://unpkg.com/simpledotcss@2.0.7/simple.min.css")
-        with body():
+        with body():  # warning, don't set the id here, does not work!
             h1("Hello {{ project_name }}")
-            with div(id_="content"):
+            with div(id_="content"):  # see target_id below.
                 # The html triggered inside your views will appear here.
                 yield
 
@@ -31,13 +31,29 @@ def content_template(encrypted_message=None):
     This template is specific to your view and the callbacks belonging to it.
     """
     p("Top secret agent? Encrypt your message with a super secret key:")
-    input_(id_="message", oninput=callback(my_callback, THIS))
+    label("Scream?")
+    uppercase = input_(type_="checkbox", id_="uppercase")
+    input_(id_="message", oninput=callback(my_callback, THIS,
+                                           uppercase))  # call "my_callback" on each oninput event.
     pre(code(encrypted_message if encrypted_message else "Type something, dammit!"))
+
+# Views - one view is normally an entire app with callbacks as actions.
 
 @hypergen_view(perm=NO_PERM_REQUIRED, base_template=base_template)
 def my_view(request):
+    """
+    Views renders html.
+    """
     content_template()
 
+# Callbacks - if you have a lot, move them to a callbacks.py file.
+
 @hypergen_callback(perm=NO_PERM_REQUIRED, target_id="content")
-def my_callback(request, message):
-    content_template(codecs.encode(message, 'rot_13'))
+def my_callback(request, message, uppercase):
+    """
+    Tells the frontend to put the output of content_template into the 'content' div.
+    """
+    encrypted_message = codecs.encode(message, 'rot_13')
+    if uppercase:
+        encrypted_message = encrypted_message.upper()
+    content_template(encrypted_message)
