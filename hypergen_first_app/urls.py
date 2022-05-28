4da5a4ede6f861e3d0297f9364f565b2a702cc87
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-28 19:39:53 +0200

    remove example app.

--- hypergen_first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from hypergen.contrib import hypergen_urls
-from hypergen_first_app import views
-
-app_name = 'hypergen_first_app'
-
-# Automatically creates urlpatterns for all functions in views.py decorated with @hypergen_view or @hypergen_callback.
-# If that's not your thing, normal urlpatterns works as well.
-urlpatterns = hypergen_urls(views, namespace="hypergen_first_app")
